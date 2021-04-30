5737bb959a593f18f68be29022673f4f0f6ac060
Dr. Michael J. Reale <realemj@sunypoly.edu>
2021-04-29 20:17:00 -0400

    MINE

---
+++ src/main/java/edu/realemj/Apple.java
@@ -0,0 +1,16 @@
+package edu.realemj;
+
+public class Apple {
+
+    public Apple() {
+
+    }
+
+    public void yak() {
+        
+    }
+
+    public String toString() {
+        return "apple";
+    }
+}
